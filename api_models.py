a15a07a3b24544372a69f171b69893771f4f5f32
vahe ghazaryan <vahe.a.ghazaryan@ameriabank.am>
2025-10-06 20:20:17 +0400

    person_id and event_id for each paymentconfirmresponse

--- api_models.py
+++ api_models.py
@@ -310,6 +310,8 @@ class PaymentConfirmRequest(BaseModel):
 
 class PaymentConfirmResponse(BaseModel):
     order_id: int
+    person_id: UUID
+    event_id: UUID
     provider: PaymentProvider
     payment_id: Optional[UUID] = None
     status: PaymentStatus
